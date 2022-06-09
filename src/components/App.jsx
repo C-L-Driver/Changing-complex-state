8ddecb69a33c541a38aa9c218762268dcd5131c4
C-L-Driver <69204245+C-L-Driver@users.noreply.github.com>
2022-06-09 10:06:03 +0100

    getting there

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form submit={fullName}>
+        <input
+          onChange={handleChange}
+          value={fullName.fName}
+          name="fName"
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          value={fullName.lName}
+          name="lName"
+          placeholder="Last Name"
+        />
+
         <button>Submit</button>
       </form>
     </div>
